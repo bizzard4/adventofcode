50c431a83fb505d233c7380188e1dc5814b329a7
François Gingras <francois.gingras89@gmail.com>
2015-12-16 16:35:08 -0500

    Advent of code - A4

---
+++ AdvendOfCode/src/A4.java
@@ -0,0 +1,52 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
+public class A4 {
+
+	public static void main(String[] args) {
+		try
+		{
+			// Key
+			String input = "iwrupvqb";
+			
+			int i = 1;
+			boolean found = false;
+			
+			// Bruteforce search
+			while (!found) {
+				String toHash = input + i;
+				String md5 = getMD5(toHash);
+				
+				// Win condition
+				if (md5.startsWith("00000")) {
+					found = true;
+					System.out.println(toHash);
+				}
+				
+				// Flow tracer
+				if ((i % 1000) == 0) {
+					System.out.println(i);
+				}
+				
+				i++;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getMD5(String s) {
+		MessageDigest md5;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+			return (new HexBinaryAdapter()).marshal(md5.digest(s.getBytes()));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
